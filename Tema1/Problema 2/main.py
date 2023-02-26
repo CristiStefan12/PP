dce78144a81f846826ec03087529e6b8c3425408
Cristian-Mihai Ștefan <crististefan26@gmail.com>
2023-02-26 20:41:23 +0200

    Eliminare Spatii Multiple

--- Tema1/Problema 2/main.py
+++ Tema1/Problema 2/main.py
@@ -1,7 +1,10 @@
+
 f = open("data.txt", "r")
 change = f.read()
 for repl in [".", ",", "?","!", "-", "_",":", ";", "/","(", ")", "'",'"']:
     change = change.replace(repl,"")
-
+change = " ".join(change.split())
 print(change)
 
+
+f.close()
